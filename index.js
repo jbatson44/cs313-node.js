48fa72f1debd4972b16f10ed9e779615d04007b8
jbatson44 <bat14009@byui.edu>
2018-06-23 11:42:26 -0600

    fix

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-var express = require('express')
-var app = express();
-var url = require('url');
-//const path = require('path')
-/*const PORT = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
-app.get('/getRates', function(request, response) {
-	getRates(request, response);
-});
-
-function getRates(request, response) {
-	
-}
-app.listen(8888, () => console.log('Example app listening on port 3000!'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
